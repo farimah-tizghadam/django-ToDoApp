b4ddddf927fcccc55b88d3a61705f5ed504d8ebc
Farimah <108592778+farimah-tizghadam@users.noreply.github.com>
2025-10-13 14:37:01 +0330

    generating fake data
    + create custom command  for generating fake data

---
+++ core/todo/management/commands/insert_data.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+import random
+from datetime import datetime
+
+from accounts.models import User, Profile
+from todo.models import Task
+
+category_list = [
+    "IT",
+    "Design",
+    "Fun",
+    "IOT",
+]
+
+
+class Command(BaseCommand):
+    help = "inserting dummy data"
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        user = User.objects.create_user(email=self.fake.email(), password='Test@123456')
+        profile = Profile.objects.get(user=user)
+
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.description = self.fake.paragraph(nb_sentences=5)
+        profile.save()
+
+        for _ in range(5):
+            Task.objects.create(
+            user = profile,
+            title = self.fake.paragraph(nb_sentences=1)[:-1],
+            complete = random.choice([True, False]),
+        )

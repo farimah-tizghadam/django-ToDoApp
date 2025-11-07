6af246b31f5a5b3d23d6dbd7299a0cd5d73a7425
Farimah <108592778+farimah-tizghadam@users.noreply.github.com>
2025-11-07 17:04:42 +0330

    configured django for postgres

--- core/core/settings.py
+++ core/core/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'todoapp-db',
-        "USER":'admin',
-        "PASSWORD":'a/@123456',
-        "HOST": 'postgres',
-        "PORT": '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432'
     }
 }
 
